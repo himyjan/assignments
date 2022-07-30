19f9ce1836c29b048d87b5f073caf212cc6724eb
Your Name <you@example.com>
2022-07-30 12:14:29 +0800

    part2 2022_07_28 java script assiment

---
+++ part-3_week2/reacttypescript/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+    fs: {
+      strict: false,
+    },
+  },
+  plugins: [react()],
+});
