332aef6a7361d09899ab3fec81d0076380e04923
Your Name <you@example.com>
2022-07-30 19:26:38 +0800

    part2 2022_07_28 java script assiment

--- part-3_week2/nexttypescript/build/README.md
+++
@@ -1,4 +0,0 @@
-# AppWorkSchool_FrontEndTraining
----
-
-next create-t3-app project deploy by vercel
--- part-3_week2/nexttypescript/next.config.js
+++ part-3_week2/nexttypescript/next.config.js
@@ -1,18 +1,7 @@
-import { env } from "./src/env/server.mjs";
-
-/**
- * Don't be scared of the generics here.
- * All they do is to give us autocompletion when using this.
- *
- * @template {import('next').NextConfig} T
- * @param {T} config - A generic parameter that flows through to the return type
- * @constraint {{import('next').NextConfig}}
- */
-function defineNextConfig(config) {
-  return config;
-}
-
-export default defineNextConfig({
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-});
+}
+
+module.exports = nextConfig
