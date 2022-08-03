68b7cd5ac2b632ff0a873a9088f76f6f7cf16894
Your Name <you@example.com>
2022-08-03 16:47:07 +0800

    part4 2022_08_03 npm express assiment

--- part-4_week2/Advance Assignment 1 (HTTP Cookie)/README.md
+++ part-4_week2/Advance Assignment 1 (HTTP Cookie)/README.md
@@ -3,7 +3,7 @@
 Sample Todo App using cookie-parser
 # How to run
 - run `node index.js`
-- open a browser on http://127.0.0.1:5000
+- open a browser on http://127.0.0.1:3000
 - Open the Chrome inspector and switch to the Application tap > Storage > cookies > http://127.0.0.1
 - Add tasks using the form and observe the content of the tasks cookie
 - As an exercise add a button to remove the task from the cookie
--- part-4_week2/Advance Assignment 1 (HTTP Cookie)/app.js
+++ part-4_week2/Advance Assignment 1 (HTTP Cookie)/app.js
@@ -8,22 +8,20 @@ app.use(express.urlencoded({ extended: false }));
 templateEngin.configure("views", {
   express: app,
 });
+app.route("/myName").get((req, res) => {
+  res.render("myName.html", { myNames: req.cookies.myNames });
+});
 app
-  .route("/")
+  .route("/trackName")
   .get((req, res) => {
-    res.render("list.html", { tasks: req.cookies.tasks });
+    res.render("trackName.html", { myNames: req.cookies.myNames });
   })
   .post((req, res) => {
-    const listItems = req.cookies.tasks ?? [];
+    const listItems = req.cookies.myNames ?? [];
     listItems.push(req.body.title);
-    res.cookie("tasks", listItems);
-    res.redirect("/");
+    res.cookie("myNames", listItems);
+    res.redirect("/myNames");
   });
-app.listen(5000, () => {
-  console.log("listening on http://127.0.0.1:5000");
+app.listen(3000, () => {
+  console.log("listening on http://127.0.0.1:3000");
 });
-
-// auto close node process
-setTimeout(() => {
-  exit(0);
-}, 20000);
