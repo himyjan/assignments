c0c7624e78bd8bdfec2477b1e43fa6c94c7d3d93
Your Name <you@example.com>
2022-08-03 20:44:51 +0800

    part4 2022_08_03 npm express assiment

--- part-4_week2/Advance Assignment 1 (HTTP Cookie)/app.js
+++ part-4_week2/Advance Assignment 1 (HTTP Cookie)/app.js
@@ -8,25 +8,23 @@ app.use(express.urlencoded({ extended: false }));
 templateEngin.configure("views", {
   express: app,
 });
-app.route("/myName").get((req, res) => {
-  res.render("myName.html", { myNames: req.cookies.myNames });
-});
 app
-  .route("/trackName")
+  .route("/myName")
   .get((req, res) => {
-    let { name } = req.query;
-    if (name == undefined) {
-      res.render("trackName.html", { myNames: req.cookies.myNames });
+    if (
+      req.cookies.myNames == undefined ||
+      req.cookies.myNames == "undefined"
+    ) {
+      res.render("trackName.html", { myNames: `${req.cookies.myNames}` });
     } else {
-      res.send(`Hello, ${name}!`);
+      res.render("myName.html", { myNames: `${req.cookies.myNames}` });
     }
   })
   .post((req, res) => {
-    const listItems = req.cookies.myNames ?? [];
-    listItems.push(req.body.title);
-    res.cookie("myNames", listItems);
+    res.cookie("myNames", req.body.title);
     res.redirect("/myName");
   });
+
 app.listen(3000, () => {
   console.log("listening on http://127.0.0.1:3000");
 });
@@ -39,4 +37,3 @@ app.listen(3000, () => {
 // opens the url in the default browser
 const open = require("open");
 open("http://localhost:3000/myName");
-open("http://localhost:3000/trackName");
